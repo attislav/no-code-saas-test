510973b6fc0a1b32962326834c15cccaf4fba74c
attislav <info@tripleadigital.de>
2025-07-23 23:50:36 +0200

    Use service_role key for frontend temporarily to fix access

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://cngwfskwxtafyizccbtm.supabase.co'
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNuZ3dmc2t3eHRhZnlpemNjYnRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzMDQzNDgsImV4cCI6MjA2ODg4MDM0OH0.mgYRg7kzc9i2kzLM2kZaL2pcRBeKObLfu-JL7ShoDyo'
+const supabaseUrl = 'https://cngwfskwxtafyizccbtm.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNuZ3dmc2t3eHRhZnlpemNjYnRtIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MzMwNDM0OCwiZXhwIjoyMDY4ODgwMzQ4fQ.ByC8CfSFwHEEQD-HWo5_NNr3SmsS6hcWinzO2o1pSh0'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
